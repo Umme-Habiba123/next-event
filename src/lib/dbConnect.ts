0c0910ce9606ff470f5bb7397dc302d50ec6b6e6
Umme-Habiba123 <habibaislammim@gmail.com>
2026-01-27 20:36:08 +0600

    fine database by client

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,3 +1,14 @@
 import { MongoClient } from "mongodb";
 
- 
+if(!process.env.MONGODB_URI){
+      throw new Error("No mongoDB uri found")
+}
+
+const uri = process.env.MONGODB_URI
+
+async function dbConnect(){
+      const client = new MongoClient(uri)   //database connect korbe--- 
+    await client.connect()   //client database er access dibe------
+    const db=client.db("next_event")  //databse pele return---
+    return {client,db};
+}
