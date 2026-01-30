d0dd3268653a8869b5b219729450ca1e391b9b35
Umme-Habiba123 <habibaislammim@gmail.com>
2026-01-30 23:55:47 +0600

    edde mongoDB server

--- src/lib/mongoConnect.ts
+++ src/lib/mongoConnect.ts
@@ -1,5 +1,8 @@
 import { MongoClient,Db } from "mongodb";
+import clientPromise from "./mongodb";
 
-function mongoClient (): Promise<{client:MongoClient,db:Db}>{
- const client
+export async function mongoClient (): Promise<{client:MongoClient,db:Db}>{
+ const client = await clientPromise
+ const db=client.db('nextEvent')
+ return { client,db}
 }
